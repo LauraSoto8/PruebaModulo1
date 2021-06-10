e18229c14e81a8e7ec93e6b595ec402779286105
Laura soto <lauraasotorojas@gmail.com>
2021-06-09 22:07:39 -0400

    modificaciones letra

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,16 @@
-$(window).on("scroll", function(){
-    if($(document).scrollTop() >10)
-      {
-        $('.navbar').addClass('navbar-dark bg-dark');
-    }
-  });
 
   $( document ).ready(function(){
     $('.carousel-inner').carousel({
       interval: 2000
     })
+});
+
+var nav = document.querySelector('nav');
+ 
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 10) {
+    nav.classList.add('bg-dark');
+  } else {
+    nav.classList.remove('bg-dark');
+  }
 });
